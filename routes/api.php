2da945f84ce0b6223b46ae627b0c9423cf7d4c80
Isroilov Azizbek <aziz16110904@gmail.com>
2024-09-20 16:42:22 +0500

    completed Product translate

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);
+Route::apiResource('products', ProductController::class);
